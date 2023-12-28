8e379cedc7fcc18ede149f2e894e6c3615264ee9
joshua <joshvigel329@outlook.com>
2023-12-28 11:27:50 -0600

    changes

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -26,11 +26,11 @@ const ProjectPage = () => {
                                 "I wish I was like this instead of that". </h1>
 
                                 <h1> I hope it is not presumptious of me to assume that I have gone through a fair share of problems and tragedies in my own life, and 
-                                unsurprisingly have many areas in my life that I feal inadequate in. For example, I am not very good at competitive math, and that trait is something that I somewhat despise.</h1>
+                                unsurprisingly have many areas in my life that I feal inadequate in.</h1>
 
                                 <h1> Regardless of my desire to become certain things or my dislike of certain traits, or my regrets about past events or decisions, if someone gave me the option to 
                                 live the life of someone else who had every trait that I longed for and never went through the times I did, I would resolutely refuse. If I had the option to live the life of 
-                                someone smart like Tarence Tao, Albert Einstien, (any other well accomplished person), or maybe someone who lived a life of utter comfort and avoided the mistakes and 
+                                someone smart like Bill Gates, Albert Einstien, or maybe someone else who lived a life of utter comfort and avoided the mistakes and 
                                 tragedies I went through, I would again refuse. Why? Do I not desire to become those traits and rid myself of those shameful decisions and experiences? </h1>
 
                                 <h1> For me, the answer is that there is value in the things I have experienced. From the outside, my life is not nearly the most successful or admirable, there are people my 
@@ -38,9 +38,9 @@ const ProjectPage = () => {
                                 experience is priceless compared to some perceived "value" of life based off of success and accomplishment. </h1>
                             
                                 <h1> It all boils down to what I really want out of life. And to me, once you can escape a your own head and try to look at life objectively, you realize that 
-                                life really is nothing but what we experience. Sure, there are people more successful than me, but on our deathbeds, the only thing we will remember are the lessons we learned,
-                                the thoughts we had, the people we met, and our collective experiences that shaped who we became. Consider a person who made a terrible mistake and spent their entire life in 
-                                prison, or a person who grew up poor and was never able to escape poverty, or a baby that only got to live 2 years before dying of a disease. All of those lives seem tragic, and
+                                life really is nothing but what we experience. Sure, there are people more successful than me, but after we die, the only thing that really "exists" of our lives is our 
+                                experiences. Consider a person who made a terrible mistake and spent their entire life in prison, or a person who lived their life in complete poverty, or a baby that only got to 
+                                live 2 years before dying of a disease. All of those lives seem tragic, and
                                 yet they still gained something from what we call "life", their experience. At the end of their life, they still had some nuanced understanding of the world they lived in,
                                 a worldview that was likely never shared exactly by anyone ever before nor will ever be held again (to an extent). Each was able to touch a different part of truth, regardless of 
                                 how sad or unaccomplished they were. </h1>
@@ -50,7 +50,7 @@ const ProjectPage = () => {
                                 else after me will (again, to an extent). To live the life of a billionare, but to have never known what life really is, to me is a waste. </h1>
 
                                 <h1> The main motivation for this whole topic was a question that I thought, that if someone offered me a pill that would make me immortal, would I take it? Would I want to live
-                                forever? Maybe my answer sounds rediculus, but no, I would not, and for the simple reason that I want to know what death is like. There has not been a single human that was able
+                                forever? No, I would not, and for the simple reason that I want to know what death is like. There has not been a single human that was able
                                 to escape death, and there has been nobody to come back after dying to tell about how it went. Again, life to me is about experience, and death itself, is one of the most
                                 mysterious. Moments before I die, will my life flash before my eyes? Will it all just go quietly? I do not know, but after those moments, <i> I shall know, </i> and I think then 
                                 I will have learned more about what life really is, even if for a short moment. I can die peacefully knowing that in some way I discovered something.</h1>
